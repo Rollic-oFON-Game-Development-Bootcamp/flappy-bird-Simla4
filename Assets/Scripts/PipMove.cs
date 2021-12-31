2cc20185b456208d82179426b05d40eaad39a22d
Simla Beken <38881186+Simla4@users.noreply.github.com>
2021-12-31 17:39:33 +0300

    feat: move the pipes

---
+++ Assets/Scripts/PipMove.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipMove : MonoBehaviour
+{
+    [SerializeField] private float speed = 0.5f;
+    [SerializeField] private float time = 10;
+
+    private void Start()
+    {
+        Destroy(gameObject, time);
+    }
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * speed);
+    }
+}
