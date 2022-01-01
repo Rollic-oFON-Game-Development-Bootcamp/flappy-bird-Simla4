beda510f880e0677984b469666b2de722db1e4a7
Simla Beken <38881186+Simla4@users.noreply.github.com>
2022-01-02 02:09:43 +0300

    feat: game over and start buttons

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] Text tScote;
+    [SerializeField] private Text tScote;
+    [SerializeField] private UIManager uIManager;
+    
 
     public int score = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 0;
     }
 
     // Update is called once per frame
@@ -26,4 +29,17 @@ public class GameManager : MonoBehaviour
         score++;
         return tScote.text = score.ToString();
     }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void StartGame()
+    {
+        uIManager.StartGame();
+        Time.timeScale = 1;
+    }
+
+    
 }
